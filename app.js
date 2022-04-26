599953c7866d45e1215505f13c714adf4a697efd
Amon <102616376+fdnakn@users.noreply.github.com>
2022-04-27 02:03:35 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const boxes = document.querySelectorAll(".box");
+
+
+window.addEventListener("scroll",checkboxes);
+
+
+
+function checkboxes(){
+    const triggerbottom = window.innerHeight/5*4;
+    
+    boxes.forEach(box=>{
+        const boxtop = box.getBoundingClientRect().top;
+        
+        if(boxtop<triggerbottom){
+            box.classList.add("show");
+        }else{
+            box.classList.remove("show");
+        }
+    })
+}
